d364979191c039a35105d0e28af144850ce63b83
jlsantia21 <jlsantia21@gmail.com>
2023-11-25 18:59:49 -0800

    Completed assignment up to step 3. Need to implement node server because import and export are not working without it. Tested functions so far by declaring studentData within studentTable, so things should work if I just get the import and export working.

---
+++ studentData.js
@@ -0,0 +1,27 @@
+// array of student objects with following properties
+// id: A unique student identifier
+// name: The student's name
+// address: The student's address
+// averageGrade: The student's average
+const students = [{id: 1,
+                    name: "Bob",
+                    address: "1234 Way",
+                    averageGrade: 72},
+                {id: 2,
+                    name: "Alice",
+                    address: "1234 Over",
+                    averageGrade: 95},
+                {id: 3,
+                    name: "John",
+                    address: "1234 There",
+                    averageGrade: 79},
+                {id: 4,
+                    name: "Sally",
+                    address: "1234 No",
+                    averageGrade: 88},
+                {id: 5,
+                    name: "May",
+                    address: "1234 Where",
+                    averageGrade: 81}];
+
+export { students }; // export students to be used in studentTable.js
---
+++ studentTable.js
@@ -0,0 +1,98 @@
+// import student data from studentData.js
+import { students } from "./studentData.js";
+
+// function to render student table from table element and array of student data 
+// table: a reference to an HTML table where data will be displayed
+// data: student data from studentData.js
+function renderStudentTable(table, data) {
+    // create thead to append to table
+    const thead = document.createElement("thead");
+    const trow = document.createElement("tr"); // create a table row
+
+    // array to store table headers
+    const headers = ["ID", "Name", "Address", "Average Grade"];
+
+    // iterate through headers  array and create table headers
+    for (let i = 0; i < headers.length; i++) {
+        const th = document.createElement("th");
+        th.innerText = headers[i];
+        trow.appendChild(th);
+    }
+
+    // append table row of headers to table head
+    thead.appendChild(trow);
+
+    // append thead to table element
+    table.appendChild(thead);
+
+    // create a tbody element to append our new rows to
+    const tbody = document.createElement("tbody");
+
+    // iterate through student data
+    for (let i = 0; i < data.length; i++) {
+        // call renderRow function to create a row of our student object
+        renderRow(tbody, data[i]);
+    }
+
+    // append tbody to table
+    table.appendChild(tbody);
+}
+
+// helper function that renders a row of the table
+// tbody: the table body element in our HTML
+// student: the current student object we are making into a row
+function renderRow(tbody, student) {
+    // test print student object
+    console.log(student);
+
+    // create row to add data to
+    const tr = document.createElement("tr");
+
+    // iterate through key value pairs in our student object
+    for (const [key, value] of Object.entries(student)) {
+        console.log(`${key}: ${value}`);
+        if (key === "averageGrade") { // if we are in the avaerageGrade column
+            const td = document.createElement("td");  // create new td
+
+            // add an input text box to the table data
+            const input = document.createElement("input"); // add input element
+            input.type = "text"; // set input to type text
+            input.value = value; // set default value
+
+            // add event listener to input
+            input.addEventListener("change", updateGrade);
+
+            td.appendChild(input); // append input to td 
+            tr.appendChild(td); // append td to tr
+        } else {
+            const td = document.createElement("td");
+            td.innerText = value; // store student's property in td
+            tr.appendChild(td); // append td to tr
+        }
+    }
+
+    // append table row to tbody
+    tbody.appendChild(tr);
+}
+
+function updateGrade(){
+    console.log("calling updateGrade() function");
+}
+
+// testing function ******************************************
+
+for(let i = 0; i < students.length; i++){
+    console.log(students[i]);
+}
+
+// get student-table table from index.html
+const studentTable = document.getElementById("student-table");
+
+// get button that triggers renderStudentTable function
+const renderTable = document.getElementById("render-table");
+
+// add event listener to trigger renderStudentTable funciton
+// when button is clicked
+renderTable.addEventListener("click", function () {
+    renderStudentTable(studentTable, students);
+});
