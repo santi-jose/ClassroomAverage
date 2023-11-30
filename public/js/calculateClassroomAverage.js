fd3a12f8e14788eb560d8f4be4fe9d1ed74aaf70
jlsantia21 <jlsantia21@gmail.com>
2023-11-29 21:00:44 -0800

    Added higher order funciton forEach to help find average of classroom grades.

--- public/js/calculateClassroomAverage.js
+++ public/js/calculateClassroomAverage.js
@@ -6,11 +6,10 @@ function calculateClassroomAverage(students){
     // student grades
     let sum = 0;
 
-    // iterate through students in array
-    for(const student of students){
-        // calculate sum of student grades
+    // iterate through students in array and get sum of average grades
+    students.forEach((student) => {
         sum = sum + student.averageGrade;
-    }
+    })
 
     // calculate average by dividing by students.length
     const avg = sum/students.length;
